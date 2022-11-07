48991f3357803687e679331188aae47e779b12ea
Cedric <im.mikepineda@gmail.com>
2022-11-08 05:26:00 +0800

    Added initial Footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+
+
+//@ts-ignore
+function Footer(props) {
+  const styles = {
+    footerContainer: {
+      height: '200px',
+      backgroundColor: '#06283D',
+    }
+  }
+
+  return (
+    <Box sx={styles.footerContainer}>
+      
+    </Box>
+  );
+}
+
+export default Footer;
