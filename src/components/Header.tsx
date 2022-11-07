f882433434bd28921c5932292e817d031befd49a
Cedric <im.mikepineda@gmail.com>
2022-11-08 05:25:47 +0800

    Added initial Header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+import { theme } from '..';
+
+
+//@ts-ignore
+function Header(props) {
+  const style = {
+    height: '65px',
+  }
+
+  return (
+    <Box sx={style}>
+    </Box>
+  );
+}
+
+export default Header;
