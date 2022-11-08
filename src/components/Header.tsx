80c437c30fa126b3daf34ae6f620743975ef880c
Cedric <im.mikepineda@gmail.com>
2022-11-08 19:41:54 +0800

    Initial header commits.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,30 @@
 import { Box } from '@mui/material';
-import { theme } from '..';
+import MenuIcon from '@mui/icons-material/Menu';
+import { StyleSharp } from '@mui/icons-material';
 
 
 //@ts-ignore
 function Header(props) {
-  const style = {
-    height: '65px',
+  const styles = {
+    headerContainer: {
+      height: '65px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingTop: '15px',
+      paddingBottom: '15px',
+      paddingRight: '30px',
+      paddingLeft: '30px',
+    },
+    menuIcon: {
+      color: '#4C81C7',
+      fontSize: '35px',
+    },
   }
 
   return (
-    <Box sx={style}>
+    <Box sx={styles.headerContainer}>
+      <span>ETC LOGO</span>
+      <MenuIcon sx={styles.menuIcon}/>
     </Box>
   );
 }
